0f05d4c4b75f097f64a2e86ba55d485eb2dfef75
김정오 <kismi5jung@gmail.com>
2015-10-21 23:44:38 +0900

    Introduce initial server code
    Make new directory client and server
    Add new package.json and server.js for server behaving
    
    Usage for server running  :
    cd server; npm install; node server.js

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
+
+app.post('/createPoll', function (req, res) {
+  console.log("body: " + JSON.stringify(req.body));
+  console.log("req.route : " + JSON.stringify(req.route));
+  res.send(req.body);
+});
+
+var server = app.listen(5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
