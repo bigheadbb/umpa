73bad9e0a55673e9092b4d3a459886139c288bc8
김정오 <kismi5jung@gmail.com>
2016-01-20 20:41:54 +0900

    [client] ask content : change total voted user icon

--- client/src/app/components/svg-icons/vote-user.jsx
+++ client/src/app/components/svg-icons/vote-user.jsx
@@ -8,7 +8,7 @@ var VoteUser = React.createClass({
   render: function () {
     return (
       <SvgIcon style={this.props.style} >
-        <path fill={this.props.color} d="M15,14C12.33,14 7,15.33 7,18V20H23V18C23,15.33 17.67,14 15,14M6,10V7H4V10H1V12H4V15H6V12H9V10M15,12A4,4 0 0,0 19,8A4,4 0 0,0 15,4A4,4 0 0,0 11,8A4,4 0 0,0 15,12Z"></path>
+        <path fill={this.props.color} d="M9,5A3.5,3.5 0 0,1 12.5,8.5A3.5,3.5 0 0,1 9,12A3.5,3.5 0 0,1 5.5,8.5A3.5,3.5 0 0,1 9,5M9,13.75C12.87,13.75 16,15.32 16,17.25V19H2V17.25C2,15.32 5.13,13.75 9,13.75M17,12.66L14.25,9.66L15.41,8.5L17,10.09L20.59,6.5L21.75,7.91L17,12.66Z"></path>
       </SvgIcon>
     );
   }
