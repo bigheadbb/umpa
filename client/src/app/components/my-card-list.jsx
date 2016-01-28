a36c14ff5cc301728484a90f761cad82d52e23d2
김정오 <kismi5jung@gmail.com>
2016-01-28 18:10:18 +0900

    [client] secret ask : Implement for secret mode ask
    Use AskHeader on My asks for target icon display
    Minor style change

--- client/src/app/components/my-card-list.jsx
+++ client/src/app/components/my-card-list.jsx
@@ -5,6 +5,7 @@ var {Avatar,
   CardActions,
   CardHeader,
   CardText,
+  Checkbox,
   FlatButton,
   TextField } = mui;
 var Colors = mui.Styles.Colors;
@@ -13,9 +14,26 @@ var AskResult = require('./ask-result.jsx');
 var VoteButton = require('./vote-button.jsx');
 var VoteUser = require('./svg-icons/vote-user.jsx');
 
-var Author = React.createClass({
+var Crowns = require('./crowns.jsx');
+var Ages = require('./ages.jsx');
+var Gender = require('./gender.jsx');
+
+var AskHeader = React.createClass({
   render: function () {
-    console.log("!!!!!!Author rendered");
+     var styles = {
+      author: {
+        width:'70%',
+        float: 'left',
+      },
+      crownContainer: {
+        width:'64px',
+        height:'72px',
+        float:'right',
+        paddingRight:'16px',
+      },
+    };
+
+    console.log("!!!!!!AskHeader rendered");
     console.log(this.props.userId.S);
     console.log(this.props.userName.S);
     console.log(this.props.date.S);
@@ -23,16 +41,64 @@ var Author = React.createClass({
     var author = this.props.userName.S;
     var yearMonthDay = new Date(parseInt(this.props.date.S)).toDateString();
     var time = new Date(parseInt(this.props.date.S)).toTimeString().split(' ')[0];
+    var readable = this.readableDate(this.props.date.S);
+    var rank = this.props.rank;
+    var age = this.props.age ? this.props.age.S : 'ALL';
+    var gender = this.props.gender ? this.props.gender.S : 'ALL';
     var profile_photo = "http://graph.facebook.com/"+userId+"/picture?type=small";
+
     return (
-      <CardHeader
-        avatar={<Avatar src={profile_photo}></Avatar>}
-        title={author}
-        subtitle={yearMonthDay+", "+time}
-        showExpandableButton={true} />
+      <div>
+        <div style={styles.author}>
+        <CardHeader
+          avatar={<Avatar src={profile_photo}></Avatar>}
+          title={author}
+          subtitle={readable+" ("+yearMonthDay+", "+time+")"}
+          showExpandableButton={true} />
+        </div>
+        <div style={styles.crownContainer}>
+          <Crowns rank={rank}/>
+          <Gender gender={gender}/>
+          <Ages age={age}/>
+        </div>
+      </div>
     );
-  }
+  },
+
+  readableDate: function(datetime) {
+    var currentTime = new Date().getTime();
+    var sec =  (currentTime - datetime) / 1000;
+    console.log("readableDate datetime : " + datetime);
+    console.log("readableDate currentTime : " + currentTime);
+
+    if(sec < 60)
+      return sec.toFixed(0) + ((sec.toFixed(0) <= 1) ? ' second' : ' seconds');
+
+    var min = sec / 60;
+    if(min < 60)
+      return min.toFixed(0) + ((min.toFixed(0) <= 1) ? ' minute' : ' minutes');
+
+    var hour = min / 60;
+    if(hour < 24)
+      return hour.toFixed(0) + ((hour.toFixed(0) <= 1) ? ' hour' : ' hours');
+
+    var day = hour / 24;
+    if(day < 7)
+      return day.toFixed(0) + ((day.toFixed(0) <= 1) ? ' day' : ' days');
+
+    var week = day / 7;
+    if(week < 5)
+      return week.toFixed(0) + ((week.toFixed(0) <= 1) ? ' week' : ' weeks');
+
+    var month = day / 30;
+    if(month < 12)
+      return month.toFixed(0) + ((month.toFixed(0) <= 1) ? ' month' : ' months');
+
+    var year = day / 365;
+    return year.toFixed(0) + ((year.toFixed(0) <= 1) ? ' year' : ' years');
+  },
 });
+
 
 var Content = React.createClass({
 
@@ -181,7 +247,15 @@ var MyCardList = React.createClass({
             key={ask.index.S}
             initiallyExpanded={true}
             style={styles.card} >
-            <Author userName={ask.userName} userId={ask.userId} date={ask.date} />
+            <AskHeader
+              userName={ask.userName}
+              userId={ask.userId}
+              date={ask.date}
+              rank={ask.rank}
+              age={ask.age}
+              gender={ask.gender}
+              secret={ask.secret} />
+            {this.makeSecretCheckBox(ask.secret)}
             <Content mainContent={ask.mainContent}
               yesContent={ask.yesContent}
               noContent={ask.noContent}
@@ -189,7 +263,7 @@ var MyCardList = React.createClass({
               noCount={ask.noCount} />
           </Card>
         );
-      });
+      }.bind(this));
     }
 
     return (
@@ -197,6 +271,29 @@ var MyCardList = React.createClass({
         {cards}
       </div>
     );
+  },
+
+  makeSecretCheckBox: function(askSecret) {
+    var secret = askSecret === undefined ? "none" : askSecret.S;
+
+    var styles = {
+      checkbox: {
+        marginLeft : "calc(100% - 100px)",
+        marginBottom: 5,
+      },
+      checkboxLabel: {
+        color: Colors.grey700
+      }
+    };
+
+    if (secret !== "none") {
+     return <Checkbox
+              label="secret"
+              defaultChecked={true}
+              disabled={true}
+              labelStyle={styles.checkboxLabel}
+              style={styles.checkbox} />;
+    }
   }
 });
 
